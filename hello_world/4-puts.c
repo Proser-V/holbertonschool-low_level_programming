2cfae7219495a8dfcc5ce07f9665cd1c563cff9b
Proser-V <10530@hotbertonstudents.com>
2025-02-17 11:49:26 +0100

    4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: 'Display some text'
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle"\n);
+	return (0);
+}

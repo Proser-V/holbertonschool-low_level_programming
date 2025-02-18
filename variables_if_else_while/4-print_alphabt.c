004e084c5fde344475edb96922617597226263de
Proser-V <10530@hotbertonstudents.com>
2025-02-18 10:57:57 +0100

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'q')
+		{
+		ch++;
+		}
+		else if (ch == 'e')
+		{
+		ch++;
+		}
+		else
+		{
+		putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}

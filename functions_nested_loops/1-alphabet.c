8078a51a548acd49b64f2de69ee4f5a3e4ef0f12
Proser-V <10530@hotbertonstudents.com>
2025-02-18 15:49:08 +0100

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: 0 On success.
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+	return (0);
+}

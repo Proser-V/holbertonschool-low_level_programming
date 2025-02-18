b3d9c7774c2452cb387ea258481cbc9dc4e3abe6
Proser-V <10530@hotbertonstudents.com>
2025-02-18 18:24:19 +0100

    8-24_hours.c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,15 +7,12 @@
 
 void jack_bauer(void)
 {
-	int hours;
-	int min;
-
-	hours = 0;
-	min = 0;
+	int hours = 0;
+	int min = 0;
 
 	for (; hours < 24; hours++)
 	{
-		for (minutes=0; min < 60; min ++)
+		for (minutes = 0; min < 60; min++)
 		{
 		_putchar ('0' + hours / 10);
 		_putchar ('0' + hours % 10);

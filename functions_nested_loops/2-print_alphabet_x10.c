5056404efaf042d635097c467dc3f5db60d0e0a8
Proser-V <10530@hotbertonstudents.com>
2025-02-18 16:09:04 +0100

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ * Return: no return
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int compteur;
+
+	compteur = 0;
+
+	while (compteur < 10)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+
+		{
+			_putchar(ch);
+		}
+	compteur++;
+	}
+	_putchar('\n');
+}

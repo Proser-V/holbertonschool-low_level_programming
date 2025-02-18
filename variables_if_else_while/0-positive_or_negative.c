9a86c467a900808f50eddd25e437f0eed9d15008
Proser-V <10530@hotbertonstudents.com>
2025-02-18 09:26:32 +0100

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -18,10 +19,11 @@ int main(void)
 	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
+
 	}
-		else if (n < 0)
-		{
-			printf("%d is negative\n", n);
-		}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
 	return (0);
 }

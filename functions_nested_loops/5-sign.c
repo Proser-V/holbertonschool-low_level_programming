cdb60fd7a664ad641136f9043093409f922b1dc1
Proser-V <10530@hotbertonstudents.com>
2025-02-18 16:52:48 +0100

    5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -8,21 +8,20 @@
 
 int print_sign(int n)
 {
-
-	if (n > '0')
+	if (n > 48)
 	{
 		return (1);
-		_putchar('+');
+		_putchar(43);
 	}
-	else if (n == '0')
+	else if (n == 48)
 	{
 		return (0);
-		_putchar('0');
+		_putchar(48);
 	}
-	else if (n < '0')
+	else if (n < 48)
 	{
 		return (-1);
-		_putchar('-');
+		_putchar(45);
 	}
 	return (0);
 }

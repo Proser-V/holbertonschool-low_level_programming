308fa6fbd9e997b5713b7f189545233e28b006f8
Proser-V <10530@hotbertonstudents.com>
2025-02-18 16:15:34 +0100

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -14,11 +14,10 @@ void print_alphabet_x10(void)
 	while (compteur < 10)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
-
 		{
-			_putchar(ch)
+			_putchar(ch);
 		}
-			_putchar('\n');
+	_putchar('\n');
 	compteur++;
 	}
 }

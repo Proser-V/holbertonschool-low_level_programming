7a18f2f8cabc1d38af0502909b50f5a1fba2fa6b
Proser-V <10530@hotbertonstudents.com>
2025-02-18 17:28:20 +0100

    7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ * @n: a number entry
+ * Return: absolute n value
+ */
+
+int print_last_digit(int n)
+{
+	int x;
+
+	x = n % 10;
+
+	return (x);
+}

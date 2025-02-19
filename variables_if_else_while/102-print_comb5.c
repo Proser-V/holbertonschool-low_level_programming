2f04f0874d068e6f166ead8b35f71a9aa0c5c4aa
Proser-V <10530@hotbertonstudents.com>
2025-02-19 16:38:42 +0100

    102-print_comb5.c

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int f; /* first number */
+	int s; /* second number */
+
+	for (f = 0; f < 99; f++)
+	{
+		for (s = f + 1; s < 100; s++)
+		{
+					putchar((f / 10) + '0');
+					putchar((f % 10) + '0');
+					putchar(' ');
+					putchar((s / 10) + '0');
+					putchar((s % 10) + '0');
+
+					if (f != 98 || s != 99)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

ccc5f8b81ce1222ecb911e7601639121f3d1d2e0
Proser-V <10530@hotbertonstudents.com>
2025-02-19 10:35:18 +0100

    11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -14,12 +14,13 @@ void print_to_98(int n)
 		if (n > 98)
 		{
 			n++;
+			printf("%d, ", n);
 		}
 		else if (n < 98)
 		{
 			n--;
+			printf("%d, ", n);
 		}
-	printf("%d, ", n);
-	}
 		_putchar('\n');
+	}
 }

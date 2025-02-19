9ab16807b6003d9b8055e2fcb2f321ae778e8a9d
Proser-V <10530@hotbertonstudents.com>
2025-02-19 14:31:34 +0100

    100-intel

---
+++ hello_world/Blogpost/Hello_world.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : Simple program using printf() function 
+ * to print a string to the stdout
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  printf("Hello, World!\n");
+  return (0);
+}

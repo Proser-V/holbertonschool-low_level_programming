fb7ce6537a6ff61a74081f7ec55642bd86d6a34e
Proser-V <10530@hotbertonstudents.com>
2025-02-21 16:28:42 +0100

    101-natural.c

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 
 /**
- * print_number - Helper function for Betty
- * @num: The number to print
+ * main - Entry point
+ *
+ * Return: Always 0
  */
 
 int main(void)
 {
-    int nat_numb, mult_three, mult_five, tot;
+	int nat_numb, mult_three, mult_five, tot;
 
-  nat_numb = 0;
-  tot = 0;
+	nat_numb = 0;
+	tot = 0;
 
-    while (nat_numb < 1024)    
-    {
-    mult_three = nat_numb % 3;
-    mult_five = nat_numb % 5;
-    
-        if (mult_five == 0 || mult_three == 0)
-        {
-        tot += nat_numb;
-        }
-    nat_numb++;
-    }
-    printf("La somme est : %d\n", tot);
+	while (nat_numb < 1024)
+	{
+	mult_three = nat_numb % 3;
+	mult_five = nat_numb % 5;
+
+		if (mult_five == 0 || mult_three == 0)
+		{
+		tot += nat_numb;
+		}
+	nat_numb++;
+	}
+	printf("La somme est : %d\n", tot);
+	return (0);
 }

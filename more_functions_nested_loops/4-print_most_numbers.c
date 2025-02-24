7f0d6783f46d672a86e2b679f81ded587796f9cc
Proser-V <10530@hotbertonstudents.com>
2025-02-24 09:54:00 +0100

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_numbers - print some digits
+ *
+ * Return: nothing
+ */
+
+void print_most_numbers(void);
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (num = 2 || num = 4)
+		{
+		continue;
+		}
+
+		_putchar(num + '0');
+	}
+	_putchar('\n');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -20,5 +20,6 @@ void print_numbers(void);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_most_numbers(void);
 
 #endif /* MAIN_H */

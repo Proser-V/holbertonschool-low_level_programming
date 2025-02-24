aff0ecf4c4d2489f58efe4384e2a0e8144861423
Proser-V <10530@hotbertonstudents.com>
2025-02-24 10:49:51 +0100

    7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print a diagonal
+ * @n: number of "\"
+ * Return: nothing
+ */
+
+void print_diagonal(int n)
+{
+	int a;
+	int s;
+
+	a = 0;
+	s = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a != n; a++)
+		{
+			if (a == 0)
+			{
+			continue;
+			}
+			else
+			{
+				while (s < n)
+				{
+				_putchar(' ');
+				s++;
+				}
+			s = 0;
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}

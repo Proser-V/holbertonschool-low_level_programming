2e754f84c00f863ce2efb007ad64dd69c4e1a1e3
Proser-V <10530@hotbertonstudents.com>
2025-02-24 11:45:20 +0100

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,30 +10,29 @@ int main(void)
 {
 	int nat_numb, mult_three, mult_five;
 
-	nat_numb = 0;
+	nat_numb = 1;
 
-	while (nat_numb < 100)
+	for (nat_numb = 1; nat_numb < 100; nat_numb++)
 	{
 	mult_three = nat_numb % 3;
 	mult_five = nat_numb % 5;
 
-		if (mult_three == 0 && mult_five != 0)
-		{
-		printf("Fizz ");
-		}
-		if (mult_five == 0 && mult_three != 0)
-		{
-		printf("Buzz ");
-		}
 		if (mult_three == 0 && mult_five == 0)
 		{
 		printf("BuzzFizz ");
+		}
+		else if (mult_five == 0 && mult_three != 0)
+		{
+		printf("Buzz ");
+		}
+		else if (mult_three == 0 && mult_five != 0)
+		{
+		printf("Fizz ");
 		}
 		else
 		{
 		printf("%d ", nat_numb);
 		}
-	nat_numb++;
 	}
 	return (0);
 }

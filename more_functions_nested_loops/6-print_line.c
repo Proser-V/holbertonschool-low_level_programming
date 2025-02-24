b2f47b6a548db16ff609c11441913cb5a74036a8
Proser-V <10530@hotbertonstudents.com>
2025-02-24 10:31:20 +0100

    6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - print a line
+ * @n: number of "_"
+ * Return: nothing
+ */
+
+void print_line(int n)
+{
+	int a;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 1; a != n; a++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}

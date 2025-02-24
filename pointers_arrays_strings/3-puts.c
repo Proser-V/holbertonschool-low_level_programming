579b9c2bc4d9a6087d75464eb723ed8687ada9cf
Proser-V <10530@hotbertonstudents.com>
2025-02-24 16:58:17 +0100

    3-puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - print a string
+ * @str: target string
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	char *i = str;
+
+	for (i = str; *i; i++)
+	{
+		_putchar(*i);
+	}
+}

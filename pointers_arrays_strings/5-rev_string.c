fd593437db6ff357a2976189671f5eea23ea64de
Proser-V <10530@hotbertonstudents.com>
2025-02-25 14:34:18 +0100

    5-rev_string.c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,19 +8,19 @@
 
 void rev_string(char *s)
 {
-	int i = 0;
-	int j = 0;
+	int i;
+	int j;
 	int len = 0;
-	int temp;
+	char temp;
 
 	while (s[len] != '\0')
 		len++;
 
-	for (i = 0, j = i - 1; s[i] != s[j]; i++, j--)
+	for (i = 0, j = len - 1; i < j; i++, j--)
 	{
-		temp = j;
-		j = i;
-		i = temp;
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
 	}
 	_putchar('\n');
 }

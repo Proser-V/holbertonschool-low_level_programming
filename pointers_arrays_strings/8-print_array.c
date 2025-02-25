bd1e142f48ee257ba5d7731a8e92c8f943068ada
Proser-V <10530@hotbertonstudents.com>
2025-02-25 16:23:20 +0100

    8-print_array.c

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_array - print elements of an array of integers
+ * @a: target array
+ * @n: number of elements of the array to be printed
+ * Return: nothing
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+	int num;
+
+	for (i = 0; i < n; i++)
+	{
+	printf("%d, ", a)
+	}
+	_putchar('\n');
+}

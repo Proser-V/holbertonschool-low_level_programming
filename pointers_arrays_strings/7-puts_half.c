34c40828de0fd721e8d4c2b1ca3262051600ad3c
Proser-V <10530@hotbertonstudents.com>
2025-02-25 15:12:20 +0100

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - print second half of a string
+ * @str: target string
+ * Return: nothing
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int n;
+	int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	for (i = (len / 2); str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+		if ((len % 2) != 0)
+		{
+		n = (len + 1) / 2;
+		_putchar(n);
+		}
+
+	_putchar('\n');
+}

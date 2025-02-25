d90e8966fc3e849a6c756605bcb196ed22a224c0
Proser-V <10530@hotbertonstudents.com>
2025-02-25 15:43:56 +0100

    7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -16,14 +16,21 @@ void puts_half(char *str)
 
 	while (str[len] != '\0')
 		len++;
-
-	for (i = (len / 2); str[i] != '\0'; i++)
-	{
-		_putchar(str[i]);
-	}
 	if ((len % 2) != 0)
 	{
+		for (i = (len / 2) + 1; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
 	_putchar(n);
+	}
+
+	else
+	{
+		for (i = (len / 2) + 1; str[i] != '\0'; i++)
+		{
+		_putchar(str[i]);
+		}
 	}
 	_putchar('\n');
 }

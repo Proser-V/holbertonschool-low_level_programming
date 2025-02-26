415509b274c335f387c9f395fe3cc5f02525c456
Proser-V <10530@hotbertonstudents.com>
2025-02-26 14:56:51 +0100

    2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copy some characters from a string to an array
+ * @dest: destination array
+ * @src: source string
+ * @n: number of copied characters from source
+ * Return: a pointer to the resulting string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0' && dest[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (src[i] < n)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}

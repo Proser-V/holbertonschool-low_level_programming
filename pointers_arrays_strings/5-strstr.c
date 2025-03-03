81c3705b53a10ad8f3b9653d010b607b4a7f7a1c
Proser-V <10530@hotbertonstudents.com>
2025-03-03 17:29:07 +0100

    5-strstr.c

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * *_strstr- search for the first occurence of string2 in string1
+ * @haystack: target string
+ * @needle: string to find
+ * Return: pointer to the beggining of the located substring, or NULL if none
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	if (*needle == '\0')
+	{
+		return (haystack);
+	}
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (j = 1; needle[j] != '\0' && needle[j] == haystack [i + j]; j++)
+			{
+
+			}
+			if (needle[j] == '\0')
+			{
+				return (haystack + i);
+			}
+		}
+	}
+	return (NULL);
+}

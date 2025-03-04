861ae29ad246fddcdd393e09f9558eb554d9242e
Proser-V <10530@hotbertonstudents.com>
2025-03-04 17:45:56 +0100

    100-is_palindrome.c

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -1,50 +1,57 @@
 #include "main.h"
 
 /**
- * _rev_recursion - check a string in reverse
- * @s: string to print
- * Return: nothing
+ * _check_recursion - Helper function to check if a string is a palindrome
+ * @s: String to check
+ * @start: Starting index
+ * @end: Ending index
+ * Return: 1 if palindrome, 0 if not
  */
-
-typedef void (*Pass)(char);
-
-void _rev_recursion(char *s, Pass process_rev)
+int _check_recursion(char *s, int start, int end)
 {
-	if (*s == '\0')
+	if (start >= end)
 	{
-		return;
+	return (1);
 	}
-	_rev_recursion(s + 1, process_rev);
-	process_rev(*s);
+
+	if (s[start] != s[end])
+	{
+	return (0);
+	}
+
+	return (_check_recursion(s, start + 1, end - 1));
 }
 
 /**
- * _check_recursion - check a string
- * @s: string to print
- * Return: nothing
+ * _strlen_recursion - Helper function to calculate the length of a string recursively
+ * @s: String to calculate the length of
+ * Return: Length of the string
  */
-
-void _check_recursion(char *s, Pass process_check)
+int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return;
+	return (0);
 	}
-	process_check(*s);
-	_check_recursion(s + 1);
+	return (1 + _strlen_recursion(s + 1));
 }
 
 /**
- * is_palindrome - checking for palindrome
- * @s: string to check through
- * Return: 1 if palindrome, 0 if none
+ * is_palindrome - Checks if a string is a palindrome
+ * @s: String to check
+ * Return: 1 if palindrome, 0 if not
  */
-
 int is_palindrome(char *s)
 {
-	if (s == 0)
+	int length = _strlen_recursion(s);
+
+	if (s == NULL)
 	{
-		return (1);
+	return (0);
 	}
-	return (is_palindrome(_rev_recursion(s, Pass process_rev)));
+	if (length == 0)
+	{
+	return (1);
+	}
+	return (_check_recursion(s, 0, length - 1));
 }

1d865441cbc3fb58bfedb94b42b2ac214f5e608f
Proser-V <10530@hotbertonstudents.com>
2025-03-04 14:43:43 +0100

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array containing the arguments
+ * Return: 0 on success
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc - 1);
+
+	(void)argv;
+
+	return (0);
+}

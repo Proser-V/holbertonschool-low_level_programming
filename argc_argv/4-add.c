f643bf307ba4949c15f1f7ca19503ada739868f4
Proser-V <10530@hotbertonstudents.com>
2025-03-04 15:42:54 +0100

    4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array containing the arguments
+ * Return: 0 on success, 1 on Error
+ */
+
+int main(int argc, char **argv)
+{
+	int i = 1;
+	int j;
+	int result = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (isdigit(argv[i]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			j = atoi(argv[i]);
+			j += result;
+		}
+	}
+	printf("%d\n", result);
+	return (0);
+}

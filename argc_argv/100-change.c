85f3505036d5794a56bb3c07efa85ff452a0188a
Proser-V <10530@hotbertonstudents.com>
2025-03-06 17:42:09 +0100

    100-change.c

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -7,26 +7,19 @@
  * check - check conditions for main
  * @argc: number of arguments
  * @argv: array containing the arguments
- * @j : the integer value of agrv[1]
  * Return: 0 on success, 1 on Error
  */
 
-int check(int argc, char **argv, int j)
+int check(int argc, char **argv)
 {
 	int i, k;
-
-	j = atoi(argv[1]);
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	if (j < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
+
 	for (i = 1; i < argc; i++)
 	{
 		for (k = 0; argv[i][k] != '\0'; k++)
@@ -50,42 +43,35 @@ int check(int argc, char **argv, int j)
 
 int main(int argc, char **argv)
 {
-	int j = atoi(argv[1]);
-	int remains = j;
-	int count = 0;
-	int result = 0;
+	int j, remains, count = 0;
 
-	(void)argc;
+	if (check(argc, argv) == 1)
+		return (1);
 
-	while (remains >= 25)
+	j = atoi(argv[1]);
+
+	if (j < 0)
 	{
-		result = remains - 25;
-		remains = result;
-		count++;
+		printf("0\n");
+		return (0);
 	}
-	while (remains < 25 && remains >= 10)
-	{
-		result = remains - 10;
-		remains = result;
-		count++;
-	}
-	while (remains < 10 && remains >= 5)
-	{
-		result = remains - 5;
-		remains = result;
-		count++;
-	}
-	while (remains < 5 && remains >= 2)
-	{
-		result = remains - 2;
-		remains = result;
-		count++;
-	}
-	while (remains == 1)
-	{
-		count++;
-		remains--;
-	}
+
+	remains = j;
+
+	count += remains / 25;
+	remains %= 25;
+
+	count += remains / 10;
+	remains %= 10;
+
+	count += remains / 5;
+	remains %= 5;
+
+	count += remains / 2;
+	remains %= 2;
+
+	count += remains;
+
 	printf("%d\n", count);
 	return (0);
 }

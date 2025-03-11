62ae12e642c5a0f601168bf3c973435e57b184d8
Proser-V <10530@hotbertonstudents.com>
2025-03-11 14:06:37 +0100

    1-init_dog.c

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,12 +1,13 @@
 #include "dog.h"
 #include "main.h"
+
 /**
  * init_dog - initalize "dog" stucture
  * @d: dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: dog's owner's name
- *Return: nothing (void)
+ * Return: nothing (void)
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)

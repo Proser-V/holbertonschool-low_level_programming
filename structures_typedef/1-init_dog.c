cdc989f3e8b37a1ca6e95d3345e3dd20c12badaa
Proser-V <10530@hotbertonstudents.com>
2025-03-11 13:40:09 +0100

    1-init_dog.c

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include "main.h"
+/**
+ * init_dog - initalize "dog" stucture
+ * @d: dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: dog's owner's name
+ *Return: nothing (void)
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}

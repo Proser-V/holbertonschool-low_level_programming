e139657503615cc6b817f2d44839db55f2cb5d87
Proser-V <10530@hotbertonstudents.com>
2025-03-12 09:36:26 +0100

    dog.h update

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef DOG
+#define DOG
+#include <stdio.h>
 #include <stdlib.h>
 typedef struct dog
 {
@@ -13,3 +16,5 @@ typedef struct dog dog_t;
 
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
+#endif /* DOG */

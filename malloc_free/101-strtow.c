97b3104d212c4b0dede06e5c6608545c355c6341
Proser-V <10530@hotbertonstudents.com>
2025-03-12 16:38:35 +0100

    101-strtow.c

--- malloc_free/101-strtow.c
+++ malloc_free/101-strtow.c
@@ -52,6 +52,34 @@ void copy_word(char *dest, char *src, int lenght)
 }
 
 /**
+ * skip_spaces - skip all leading spaces
+ * @str: target string
+ * Return: a pointer to the first non-space character
+ */
+
+char *skip_spaces(char *str)
+{
+	while (*str == ' ')
+		str++;
+	return (str);
+}
+
+/**
+ * extract_word - calculate the lenght of the current word
+ * @str: target string
+ * Return: the lenght of words
+ */
+
+int extract_word(char *str)
+{
+	int length = 0;
+
+	while (str[length] != ' ' && str[length] != '\0')
+		length++;
+	return (length);
+}
+
+/**
  * strtow - split a string into words
  * @str: target string
  * Return: a pointer to an array of strings, or NULL if fail
@@ -60,52 +88,38 @@ void copy_word(char *dest, char *src, int lenght)
 char **strtow(char *str)
 {
 	char **n_str;
-	int index = 0, index2 = 0, lenght = 0;
+	int index = 0, index2 = 0, length = 0;
 
-	if (str == NULL || str[0] == '\0')
-	{
+	if (str == NULL || *str == '\0')
 		return (NULL);
-	}
 
+	/* Allocate memory for the word array */
 	n_str = malloc((word_counter(str) + 1) * sizeof(char *));
 	if (n_str == NULL)
-	{
 		return (NULL);
-	}
 
 	while (*str)
 	{
-		while (*str == ' ')
-		{
-			str++;
-		}
-
+		str = skip_spaces(str);
 		if (*str == '\0')
-		{
 			break;
-		}
 
-		lenght = 0;
-		while (str[lenght] != ' ' && str[lenght] != '\0')
+		length = extract_word(str);
+		if (length > 0)
 		{
-			lenght++;
-		}
-		if (lenght > 0)
-		{
-		n_str[index] = malloc((lenght + 1) * sizeof(char));
-		if (n_str[index] == NULL)
-		{
-			for (index2 = 0; index2 < index; index2++)
+			n_str[index] = malloc((length + 1) * sizeof(char));
+			if (n_str[index] == NULL)
 			{
-				free(n_str[index2]);
+				while (index > 0)
+					free(n_str[--index]);
+				free(n_str);
+				return (NULL);
 			}
-			free(n_str);
-			return (NULL);
-		}
-
-		copy_word(n_str[index], str, lenght);
-		index++;
-		str += lenght;
+			for (index2 = 0; index2 < length; index2++)
+				n_str[index][index2] = str[index2];
+			n_str[index][length] = '\0';
+			index++;
+			str += length;
 		}
 	}
 	n_str[index] = NULL;

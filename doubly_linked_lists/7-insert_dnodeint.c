e4710f5474fc06087515b753b55850c986554398
Proser-V <10530@hotbertonstudents.com>
2025-04-01 12:17:18 +0200

    7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -17,7 +17,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (node == NULL)
 	{
-		free(node);
 		return (NULL);
 	}
 	if (h == NULL)
@@ -32,7 +31,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = node;
 		return (node);
 	}
-	while (idx > 1)
+	while (idx > 0)
 	{
 		if (front == NULL)
 		{

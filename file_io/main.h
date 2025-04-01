21221f4d37f2cb4fb68b5ebfe867a97262c1541a
Proser-V <10530@holbertonstudents.com>
2025-04-01 23:29:21 +0200

    0-read_textfile.c

---
+++ file_io/main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+
+/* Prototypes */
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif

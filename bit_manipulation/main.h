9579647af53c3bf1f1ac9cd35da3f529037de31c
Proser-V <10530@hotbertonstudents.com>
2025-04-01 14:28:34 +0200

    0-binary_to_uint.c

---
+++ bit_manipulation/main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+/* Prototypes */
+unsigned int binary_to_uint(const char *b);
+
+#endif

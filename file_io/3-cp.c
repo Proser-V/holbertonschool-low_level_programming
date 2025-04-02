c41fa75d18a030c74ede1ceb4b3c79083b47df3b
Proser-V <10530@holbertonstudents.com>
2025-04-02 15:07:25 +0200

    3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -59,24 +59,15 @@ int main(int argc, char *argv[])
 	if (fd_to == -1)
 		error_exit(99, "Can't write to", argv[2], fd_from);
 
-	rd = read(fd_from, buffer, sizeof(buffer));
-	if (rd == -1)
-		error_exit(98, "Can't read from file", argv[1], fd_to);
-
-	wr = write(fd_to, buffer, rd);
-	if (wr == -1)
-		error_exit(99, "Can't write to", argv[2], fd_to);
-
-	while (rd > 0)
+	while ((rd = read(fd_from, buffer, sizeof(buffer))) > 0)
 	{
 		wr = write(fd_to, buffer, rd);
-		if (wr != rd)
-			error_exit(99, "Can't write to", argv[2], fd_to);
-
-		rd = read(fd_from, buffer, sizeof(buffer));
-		if (rd == -1)
-		error_exit(98, "Can't read from file", argv[1], fd_to);
+		if (wr == -1)
+			error_exit(99, "Can't write to", argv[2], fd_from);
 	}
+
+	if (rd == -1)
+		error_exit(98, "Can't read from file", argv[1], fd_from);
 
 	close_fd_exit(fd_from);
 	close_fd_exit(fd_to);

d5d2467206913ec76925b5e1475d95ad7ce504b1
Proser-V <10530@holbertonstudents.com>
2025-04-02 14:15:57 +0200

    Correction for 1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,7 +11,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t file, len, bytesWrite;
+	ssize_t file, bytesWrite;
 
 	if (filename == NULL)
 		return (-1);
@@ -22,10 +22,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		while (text_content[len] != '\0')
-			len++;
-
-		bytesWrite = write(file, text_content, len);
+		bytesWrite = write(file, text_content, sizeof(text_content) - 1);
 		if (bytesWrite == -1)
 		{
 			close(file);

76e0c07d92d681c6d85f16b7bcd60a6f72fce631
Proser-V <10530@holbertonstudents.com>
2025-04-02 11:15:15 +0200

    1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 {
 	ssize_t file, len, bytesWrite;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
@@ -34,6 +34,5 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	close(file);
-
 	return (1);
 }

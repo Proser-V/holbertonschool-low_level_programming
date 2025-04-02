51ea8663edb9788fdca12de60dff24df843064c5
Proser-V <10530@holbertonstudents.com>
2025-04-02 11:13:17 +0200

    1-create_file.c

--- file_io/main.h
+++ file_io/main.h
@@ -1,14 +1,15 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-#include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
 #include <fcntl.h>
+#include <sys/types.h>
 
 /* Prototypes */
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
